760c3b0a664756521b38a2aa114459f60667584c
OCorleto <34290029+OCorleto@users.noreply.github.com>
2020-03-22 17:19:12 -0600

    Actualización de script de BD

--- Arq_Servicios/SQLQuery1.sql
+++ Arq_Servicios/SQLQuery1.sql
@@ -1,6 +1,22 @@
 Create Database Fase2;
 Use Fase2;
+Create Table Usuario(
+id int auto_increment primary key,
+dpi varchar(20) not null,
+nombre varchar(100) not null,
+correo varchar(50) not null,
+direccion varchar(200) not null,
+pass varchar(50) not null,
+nacimiento date not null,
+tipouser int not null
+);
 
+
+INSERT INTO Usuario(dpi,nombre,correo,direccion,pass,nacimiento,tipouser)
+VALUES('3468231000901','Lionel Andres Messi','user','Patagonia, Argentina','123','1978-06-26',1);
+
+INSERT INTO Usuario(dpi,nombre,correo,direccion,pass,nacimiento,tipouser)
+VALUES('3697523000901','Serre Ciete','admin','Madeira, Portugal','asd','1982-02-26',0);
 Create Table Producto(
 ID numeric primary key,
 Nombre Varchar(50),
