5f3e9ae25e05048d7f9fd9dc788b73110a57c4ca
OCorleto <34290029+OCorleto@users.noreply.github.com>
2020-03-22 17:14:53 -0600

    Update funciones para Login y Registro

--- Arq_Servicios/index.js
+++ Arq_Servicios/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 const mysql = require('mysql');
+var cors = require('cors')
+const bodyParser = require('body-parser');
+app.use(bodyParser.json())
+app.use(cors())
 
 const con = mysql.createConnection({
   host     : "database-2.cxqmakqozr01.us-east-1.rds.amazonaws.com",
@@ -31,6 +35,48 @@ app.get('/products', (req, res) => {
         });
     });
 });
+
+app.get('/loginlista', (req, res) => {
+    con.connect(function(err) {
+        con.query(`SELECT * FROM Fase2.Usuario`, function(err, result, fields) {
+            if (err) res.send(err);
+            if (result) res.send(result);
+        });
+    });
+});
+
+app.get('/buscarcorreo/', function (req, res) {
+    var ext = req.query.ext;
+    var sql = "SELECT id as id,pass as pass, tipouser as tipo FROM Fase2.Usuario WHERE correo='"+ext+"';";
+    con.query(sql, function (err, result) {
+        if (err) console.log(err);
+        console.log(result)
+        res.send(result);//devolvemos que si encontro por lo menos 1
+    });
+});
+
+app.post('/registro', (req, res) => {
+    console.log(req.body);
+    console.log(req.body.dpi);
+    console.log(req.body.nombre);
+    console.log(req.body.correo);
+    console.log(req.body.pass);
+    console.log(req.body.direccion);
+    console.log(req.body.fecha);
+    if (req.body.dpi && req.body.nombre && req.body.correo && req.body.pass && req.body.direccion && req.body.fecha) {
+        console.log('Request received');
+        con.connect(function(err) {
+            con.query(`INSERT INTO Fase2.Usuario(dpi,nombre,correo,direccion,pass,nacimiento,tipouser) VALUES ('${req.body.dpi}', '${req.body.nombre}', '${req.body.correo}', '${req.body.pass}', '${req.body.direccion}', '${req.body.fecha}',1)`, function(err, result, fields) {
+                if (err) console.log(err);
+                if (result) res.send({dpi: req.body.dpi, nombre: req.body.nombre, correo: req.body.correo, pass: req.body.pass, direccion: req.body.direccion, fecha: req.body.fecha});
+                if (fields) console.log(fields);
+            });
+        });
+    } else {
+        console.log('Missing a parameter');
+    }
+});
+
 app.listen(4000, function () {
   console.log('Example app listening on port 4000!');
 });
