810044238b1c4e911a59ed94072aed970c6e9681
OCorleto <34290029+OCorleto@users.noreply.github.com>
2020-03-22 17:18:32 -0600

    Funciones login y registro

---
+++ Arq_Servicios/SitioWeb/js/loginuser.js
@@ -0,0 +1,40 @@
+
+
+function login(){
+  var correo = document.getElementById('username').value;
+  var pass = document.getElementById('password').value
+  var url ="http://localhost:4000/buscarcorreo/?ext="+correo;
+  fetch(url, {
+        method: 'GET',
+        headers:{
+            'Accept': 'application/json'
+        }
+        }).then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => console.log('Success:', Users(response,pass)));
+}
+function Users(data,contra){
+  if(data.length==0){
+    alert("El usuario no está registrado.");
+    return false;
+  }else{
+    var toJSON = data;
+    var pass
+    var tipo
+    toJSON.forEach(function(element) {
+      pass = element.pass
+      tipo = element.tipo
+    });
+    if (contra == pass){
+      if(tipo==0){
+        alert("redirigiendo a administrador")
+      }else if(tipo==1){
+        alert("redirigiendo a comprador")
+      }
+    }
+    else{
+      alert("Password incorrecta")
+      return false;
+    }
+  }
+}
---
+++ Arq_Servicios/SitioWeb/js/registrouser.js
@@ -0,0 +1,55 @@
+function setNewUser(){
+  let dpi = document.getElementById("dpi").value;
+  let nombre = document.getElementById("nombre").value;
+  let correo = document.getElementById("correo").value;
+  let pass = document.getElementById("pass").value;
+  let conf = document.getElementById("confpass").value;
+  let direccion = document.getElementById("direccion").value;
+  let fecha = document.getElementById("datenac").value;
+
+  if(pass == conf){
+    let url = 'http://localhost:4000/registro';
+        let data =  {
+            dpi: dpi,
+            nombre: nombre,
+            correo: correo,
+            pass: pass,
+            direccion: direccion,
+            fecha: fecha
+        };
+        console.log(data)
+        fetch(url, {
+            method: 'POST',
+            query: JSON.stringify(data),
+            body: JSON.stringify(data),
+            headers:{
+                "Content-Type" : "application/json"  }
+            }).then(res => console.log('Success:', limp(response)))
+            .catch(error => console.log('Error:', error))
+            .then(response => console.log('Success:', limp(response)));
+  }
+  else{
+    alert("Passwords no coinciden")
+  }
+}
+
+function limp(response){
+  //limpieza
+  alert("Usuario creado")
+  location.href ="../SitioWeb/login.html";
+  let dpi = document.getElementById("dpi").value;
+  let nombre = document.getElementById("nombre").value;
+  let correo = document.getElementById("correo").value;
+  let pass = document.getElementById("pass").value;
+  let conf = document.getElementById("confpass").value;
+  let direccion = document.getElementById("direccion").value;
+  let fecha = document.getElementById("datenac").value;
+  dpi.value=''
+  nombre.value=''
+  correo.value=''
+  pass.value=''
+  conf.value=''
+  direccion.value=''
+  fecha.value=''
+  return response;
+}
